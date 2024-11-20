1903e3acc6b444e0f9290ced249b5594efa5fdc5
Aurnab <zubayeraurnab@gmail.com>
2024-11-20 21:45:56 +0600

    many to one realtion build with post comment  example

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,16 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PhoneController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/phone', [PhoneController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/comment', [CommentController::class, 'index']);
 
 
 // Route::get('/user', function (Request $request) {
